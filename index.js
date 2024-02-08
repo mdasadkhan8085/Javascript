eaf8255182c320bcee03f6c68fd59e51caf46997
mdasadkhan8085 <mdasadkhan8085>
2024-02-08 15:43:15 +0530

    Variable Javascript

---
+++ index.js
@@ -0,0 +1,24 @@
+//Simple Print
+console.log("Hello Asad");
+
+//Variable is temperory space location
+//Simple Variable Decaration
+
+//String
+var myName = "Asad Khan";
+console.log(myName);
+
+//Number
+var phoneNumber = 8080859144;
+console.log(phoneNumber);
+
+//Boolean
+var isMale = true;
+console.log(isMale);
+
+//Double
+var pi = 3.14;
+console.log(pi);
+
+
+console.log(typeof pi);
